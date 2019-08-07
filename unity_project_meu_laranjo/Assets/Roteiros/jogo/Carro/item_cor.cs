42b58a3d51fb5fd91b5bb547d5348b72f3116892
labcaxias <labcaxias@CDCLAB_13.labdc.zentyal>
2019-08-07 19:32:35 -0100

    #upload030
    -upload de seguranca

---
+++ unity_project_meu_laranjo/Assets/Roteiros/jogo/Carro/item_cor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "item_cor_", menuName = "Laranjo/Inventario/Item de Cor")]
+public class item_cor : ScriptableObject
+{
+    public int id;
+    public string nome;
+    public string descricao;
+    public int preco;
+    public Material material;
+    public Sprite imagem;
+}
