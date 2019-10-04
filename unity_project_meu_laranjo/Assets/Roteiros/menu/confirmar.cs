510f8dcaf1ec7cea60aec1341459e45b3b1a5054
labcaxias <lahbms003@gmail.com>
2019-10-04 11:40:58 -0300

    #upload052
    -upload de seguranca

---
+++ unity_project_meu_laranjo/Assets/Roteiros/menu/confirmar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+public class confirmar : MonoBehaviour
+{
+    public string email, senha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
